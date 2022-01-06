4958a3d14491d10f377d3fbaaf87c96e1a4d0e3a
Wol <88345210+Portalwol@users.noreply.github.com>
2022-01-06 12:21:41 -0800

    bloat begon

--- 2-user.sh
+++ 2-user.sh
@@ -23,34 +23,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerleve
 ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 
 PKGS=(
-'autojump'
-'awesome-terminal-fonts'
-'brave-bin' # Brave Browser
-'dxvk-bin' # DXVK DirectX to Vulcan
-'github-desktop-bin' # Github Desktop sync
-'lightly-git'
-'lightlyshaders-git'
-'mangohud' # Gaming FPS Counter
-'mangohud-common'
-'nerd-fonts-fira-code'
-'nordic-darker-standard-buttons-theme'
-'nordic-darker-theme'
-'nordic-kde-git'
-'nordic-theme'
-'noto-fonts-emoji'
-'papirus-icon-theme'
-'plasma-pa'
-'ocs-url' # install packages from websites
-'sddm-nordic-theme-git'
-'snapper-gui-git'
-'ttf-droid'
-'ttf-hack'
-'ttf-meslo' # Nerdfont package
-'ttf-roboto'
-'ttf-ms-fonts' # microsoft fonts
-'ttf-vista-fonts' # vista fonts 
-'zoom' # video conferences
-'snap-pac'
+'firefox'
 )
 
 for PKG in "${PKGS[@]}"; do
@@ -59,10 +32,7 @@ done
 
 export PATH=$PATH:~/.local/bin
 cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
-pip install konsave
-konsave -i $HOME/ArchTitus/kde.knsv
 sleep 1
-konsave -a kde
 
 echo -e "\nDone!\n"
 exit
