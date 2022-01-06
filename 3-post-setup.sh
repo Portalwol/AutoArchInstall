5567768dd7bbb0f8cd4dc99de06a5dd4012e0e5b
Wol <88345210+Portalwol@users.noreply.github.com>
2022-01-06 12:41:22 -0800

    lightdm

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -20,12 +20,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # ------------------------------------------------------------------------
 
 echo -e "\nEnabling Login Display Manager"
-systemctl enable sddm.service
-echo -e "\nSetup SDDM Theme"
-cat <<EOF > /etc/sddm.conf
-[Theme]
-Current=Nordic
-EOF
+systemctl enable lightdm.service
 
 # ------------------------------------------------------------------------
 
